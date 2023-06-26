89c22c96166c10af5eeb1b522f32c57f7a656e30
VÕ PHAN ANH QUÂN <vophananhquan@MacBook-Pro-cua-VO.local>
2023-06-26 14:56:04 +0700

    add text Vietstock part 1

--- data/scripts/crawl_data_vietstock.py
+++ data/scripts/crawl_data_vietstock.py
@@ -1,66 +1,53 @@
 import os
 import json
+import requests
+from fake_useragent import UserAgent
 from tqdm import tqdm
+from bs4 import BeautifulSoup
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+}
 
-WEBDRIVER_DELAY_TIME_INT = 5
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
+ua = UserAgent()
+FAKE_HEADERS = {
+    'User-Agent': ua.chrome
+}
 
-driver = webdriver.Chrome('chromedriver', options=chrome_options)
+def crawl_text_and_tags(url, headers= HEADERS):
+    response = requests.get(url, headers= headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    def crawl_text():
+        nonlocal soup
+        news_content = ""
+        information = soup.find("div", {"id": "vst_detail"})
+        if information:
+            information = information.find_all("p")
+            for passage in information:
+                if passage.get('class')not in ["pTitle", "pAuthor", "pSource"]:
+                    if news_content:
+                        news_content += ' '
+                    news_content += passage.text
+        return news_content
+    def crawl_tags():
+        nonlocal soup
+        news_tags = []
+        try:
+            tag_elements = (soup.find("div", class_="tags mt20")).find_all("li")
+            if len(tag_elements) > 0:
+                for tag in tag_elements:
+                    if tag.get('class') is None:
+                        news_tags.append(tag.text.strip())
+        except:
+            print("DON'T HAVE TAGS")
+        return news_tags     
+    return crawl_text(), crawl_tags()
 
-driver.implicitly_wait(4)
-wait = WebDriverWait(driver, WEBDRIVER_DELAY_TIME_INT)
-
-def crawl_text_and_tags(url, title= None):
-    driver.get(url)
-    information_xpath = '//*[@id="vst_detail"]/p'
-    news_content = ""
-    news_tags = []
-    
-    # Get content from url
-    try: 
-        information = wait.until(EC.presence_of_all_elements_located((By.XPATH, information_xpath)))
-        for i in range(len(information)):
-            if information[i].get_attribute('class') not in ["pTitle", "pAuthor", "pSource"]:
-                if news_content:
-                    news_content += ' '
-                news_content += information[i].text
-        print("SUCCESS - content")
-    except:
-        print("FAULT - content")
-        if title:
-            print(f"FAULT - {title}")
-    
-    # Get tags from url
-    try:
-        # tags = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'tags mt20')))
-        print("1")
-        tags = driver.find_elements(By.XPATH, '//*[@class="tags mt20"]/ul/li')
-        # tags = wait.until(EC.presence_of_all_elements_located((By.XPATH, f'//*[@id="article-{url[-11:-4]}"]/div[1]/div[2]/div[2]/div[3]/ul/li')))
-        print("2", len(tags))
-        for idx in range(len(tags)):
-            if tags[idx].get_attribute('class') == 'tag_list':
-                print("Not append")
-            else:
-                news_tags.append(tags[idx].text)
-    except:
-        print("FAULT - tags")
-
-    return news_content, news_tags
-# url = 'https://vietstock.vn/2023/06/dhdcd-qcg-muc-tieu-doanh-thu-8211-loi-nhuan-2023-trai-chieu-doi-ngay-tra-co-tuc-2021-den-quy-32025-737-1081685.htm'
-# print(url[-11:-4])
-# //*[@id="article-1081685"]/div[1]/div[2]/div[2]/div[3]/ul
 with open('./data/contents/news_links_vietstock.json', 'r', encoding='utf-8') as f:
     data =json.load(f)
 
 result = []
+
 for idx in tqdm(range(len(data))):
     main_content, tags = crawl_text_and_tags(data[idx]['link'])
     dictionary = {
@@ -69,5 +56,10 @@ for idx in tqdm(range(len(data))):
         'tags': tags
     }
     result.append(dictionary)
-    if idx == 2: break
-print(result)
+    if idx == 4: break
+
+if os.path.exists('./data/contents/news_data_vietstock.json'):
+    os.remove('./data/contents/news_data_vietstock.json')
+import json
+with open('./data/contents/news_data_vietstock.json', 'w+', encoding='utf-8') as f:
+    json.dump(result, f, ensure_ascii=False, indent=4)
--- test.py
+++ test.py
@@ -3,8 +3,13 @@ import requests
 import json
 import os
 
-# url = 'https://vietstock.vn/2023/06/trs-sap-chia-co-tuc-bang-tien-ty-le-15-738-1079268.htm'
-# req = requests.get(url)
-# print(req.status_code)
+import requests
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
+
+url = 'https://vietstock.vn/2023/06/dhdcd-mpc-kinh-doanh-co-the-tot-len-tu-thang-8-nghien-cuu-cac-mo-hinh-nuoi-tom-737-1081687.htm'
+req = requests.get(url, headers= HEADERS)
+print(req.status_code)
+print(req.text)
 output = []
 print(len(output))
