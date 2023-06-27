04bf293239289590f3722fd773a0fa9d438e41f5
git <duongnghia222>
2023-06-27 18:36:18 +0700

    add hand-label

--- utils/auto_label.py
+++ utils/auto_label.py
@@ -8,24 +8,25 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY_FREE")
 openai.api_base = "https://api.chatanywhere.cn/v1"
 
-context = "Tôi sẽ đưa cho bạn một bài báo, hãy chỉ ra tên công ty được nói đến trong bài báo ấy. Sau đây là bài báo:"
+context = "Tôi sẽ đưa cho bạn một bài báo, chỉ ra mã chứng khoán của công ty được nói đến trong bài báo. Sau đây là bài báo: "
 article = ""
+post_context = ". Hãy chỉ ra mã chứng khoán của công ty được nói đến trong bài báo trên, câu trả lời chỉ 1 từ"
 
-with open('./data/contents/data_vietnambiz_1_2.json', 'r', encoding='utf-8') as f:
+with open('../data/contents/data_vietnambiz_1_1.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 
 def process(content):
     a = content.split(' ')
-    b = ' '.join(a[0:3000])
+    b = ' '.join(a[0:900])
     return b
 
-article = process(data[2]['Content'])
 
+article = process(data[4]['Content'])
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=context + article,
+  prompt=context + article + post_context,
   temperature=0,
   max_tokens=50,
   top_p=1,
---
+++ utils/hand_label.py
@@ -0,0 +1,88 @@
+import json
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+
+
+class DataVisualizer:
+    def __init__(self, data, root):
+        self.data = data
+        self.index = 0
+        self.root = root
+        self.init_text = "VIC"
+
+        self.title_text = Text(height=2, width=100, font=("Times New Roman", 12), state=DISABLED)
+        self.content_text = ScrolledText(height=20, width=100, font=("Times New Roman", 12), state=DISABLED)
+        self.tag_text = Text(height=1, width=100, font=("Times New Roman", 12), state=DISABLED)
+        self.label_entry = Entry(width=50)
+
+        self.setup_gui()
+        self.load_item()
+
+    def setup_gui(self):
+        self.root.title("Hand label")
+        self.root.geometry("800x600+160+10")  # Change the numbers as needed
+
+        title_frame = Frame(self.root)
+        title_frame.pack(fill=BOTH, expand=True)
+        Label(title_frame, text="Title").pack()
+        self.title_text.pack()
+
+        content_frame = Frame(self.root)
+        content_frame.pack(fill=BOTH, expand=True)
+        Label(content_frame, text="Content").pack()
+        self.content_text.pack()
+
+        tag_frame = Frame(self.root)
+        tag_frame.pack(fill=X, expand=True)
+        Label(tag_frame, text="Tags").pack()
+        self.tag_text.pack(fill=X, expand=True)
+
+        label_frame = Frame(self.root)
+        label_frame.pack(fill=BOTH, expand=True)
+        Label(label_frame, text="Label").pack()
+        self.label_entry.pack()
+
+        Button(label_frame, text="Save Changes", command=self.save_changes).pack()
+        Button(label_frame, text="Next", command=self.next_item).pack()
+
+        self.root.bind('<Return>', lambda event: self.save_changes())
+        self.root.bind('<Right>', lambda event: self.next_item())
+
+    def load_item(self):
+        item = self.data[self.index]
+        self.title_text.config(state=NORMAL)
+        self.title_text.delete('1.0', END)
+        self.title_text.insert('1.0', item["Title"])
+        self.title_text.config(state=DISABLED)
+
+        self.content_text.config(state=NORMAL)
+        self.content_text.delete('1.0', END)
+        self.content_text.insert('1.0', item["Content"])
+        self.content_text.config(state=DISABLED)
+
+        self.tag_text.config(state=NORMAL)
+        self.tag_text.delete('1.0', END)
+        self.tag_text.insert('1.0', ', '.join(item["Tags"]))
+        self.tag_text.config(state=DISABLED)
+
+        self.label_entry.delete(0, END)
+        self.label_entry.insert(0, self.init_text)
+
+    def save_changes(self):
+        self.data[self.index]["Label"] = self.label_entry.get()
+        print("Saved changes")
+
+    def next_item(self):
+        # self.save_changes()  # You might want to save changes before moving to the next item
+        self.index += 1
+        if self.index < len(self.data):
+            self.load_item()
+        return "break"
+
+
+with open('../data/contents/data_vietnambiz_1_1.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+root = Tk()
+app = DataVisualizer(data, root)
+root.mainloop()
